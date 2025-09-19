e124e45f518a8f4044ef0b41a00daa905d820448
mohammedalith <mohammedalith713@gmail.com>
2025-09-19 16:52:10 +0530

    'chat'

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// server.js
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 import { makeExecutableSchema } from '@graphql-tools/schema';
@@ -24,10 +23,14 @@ const typeDefs = `
 
   type Mutation {
     sendMessage(text: String!, user: String!): Message!
+    updateMessage(id: ID!, text: String!): Message!
+    deleteMessage(id: ID!): ID!
   }
 
   type Subscription {
     messageSent: Message!
+    messageUpdated: Message!
+    messageDeleted: ID!
   }
 `;
 
@@ -52,10 +55,30 @@ const resolvers = {
       pubsub.publish('MESSAGE_SENT', { messageSent: message });
       return message;
     },
+    updateMessage: (_, { id, text }) => {
+      const msg = messages.find(m => m.id === id);
+      if (!msg) throw new Error("Message not found");
+      msg.text = text;
+      pubsub.publish('MESSAGE_UPDATED', { messageUpdated: msg });
+      return msg;
+    },
+    deleteMessage: (_, { id }) => {
+      const index = messages.findIndex(m => m.id === id);
+      if (index === -1) throw new Error("Message not found");
+      messages.splice(index, 1);
+      pubsub.publish('MESSAGE_DELETED', { messageDeleted: id });
+      return id;
+    },
   },
   Subscription: {
     messageSent: {
-      subscribe: () => pubsub.asyncIterator(['MESSAGE_SENT']),
+      subscribe: () => pubsub.asyncIterableIterator(['MESSAGE_SENT']),
+    },
+    messageUpdated: {
+      subscribe: () => pubsub.asyncIterableIterator(['MESSAGE_UPDATED']),
+    },
+    messageDeleted: {
+      subscribe: () => pubsub.asyncIterableIterator(['MESSAGE_DELETED']),
     },
   },
 };
@@ -64,6 +87,7 @@ const schema = makeExecutableSchema({ typeDefs, resolvers });
 const app = express();
 const httpServer = createServer(app);
 
+// WebSocket server for subscriptions
 const wsServer = new WebSocketServer({ server: httpServer, path: '/graphql' });
 useServer({ schema }, wsServer);
 
@@ -73,4 +97,5 @@ app.use('/graphql', cors(), bodyParser.json(), expressMiddleware(server));
 
 httpServer.listen(4000, () => {
   console.log('Server running on http://localhost:4000/graphql');
+  console.log('WebSocket URL for subscriptions: ws://localhost:4000/graphql');
 });
